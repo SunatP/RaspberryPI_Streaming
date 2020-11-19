8aee2706ce0c5ab841bf96860a83503d7728714f
Sunat.P <31818562+SunatP@users.noreply.github.com>
2020-11-19 18:57:36 +0700

    update
    - Now it can measure cpu clock

--- CPUinspector/temp.py
+++ CPUinspector/temp.py
@@ -1,25 +1,29 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import sys
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(17, GPIO.OUT)
 precision = 2
 try:
     tFile = open('/sys/class/thermal/thermal_zone0/temp')
     temp = float(tFile.read())
+    cpuFile = open('/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq')
+    cpu_freq = float(cpuFile.read())
+    print('CPU Clock: ',"{:.{}f}".format(cpu_freq/1000,precision),'MHz')
     tempC = temp/1000
     if tempC > 43.5:
-        GPIO.output(17, 1)
+        # GPIO.output(17, 1)
         print('CPU Temp: ', "{:.{}f}".format(tempC, precision),str("\u00b0"),'Celsius')
         print('CPU is Warm')
-        GPIO.cleanup()
+        # GPIO.cleanup()
     else:
-        GPIO.output(17, 0)
+        # GPIO.output(17, 0)
         print('CPU Temp: ', "{:.{}f}".format(tempC, precision),str("\u00b0"),'Celsius')
         print('CPU is Cold')
-        GPIO.cleanup()
+        # GPIO.cleanup()
 except:
     tFile.close()
-    GPIO.cleanup()
+    cpuFile.close()
+    # GPIO.cleanup()
     sys.exit()
